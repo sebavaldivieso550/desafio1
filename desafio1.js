f5ad08a99271a9c666fc3e4acb8ddc597ce2bf42
sebavaldivieso550 <sebavaldivieso550@gmail.com>
2024-02-13 22:47:45 -0300

    primer commit

---
+++ desafio1.js
@@ -0,0 +1,14 @@
+class ProductManager {
+    constructor(products) {
+        this.title = title
+        this.descripcion = descripcion
+        this.precio = precio
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+
+        addProduct() {
+            
+        }
+    }
+}
