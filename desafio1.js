6ca21eeaf3c57bb7c9d7203fc49e416e8f57270f
sebavaldivieso550 <sebavaldivieso550@gmail.com>
2024-02-14 13:22:55 -0300

    Desafio terminado

--- desafio1.js
+++ desafio1.js
@@ -1,14 +1,59 @@
 class ProductManager {
-    constructor(products) {
-        this.title = title
-        this.descripcion = descripcion
-        this.precio = precio
-        this.thumbnail = thumbnail
-        this.code = code
-        this.stock = stock
+    constructor() {
+        this.products = [];
+        this.productIdCounter = 1;
+    }
+    
+    addProduct(title, description, price, thumbnail, code, stock) {
+        
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+        
+        const existingProduct = this.products.find(product => product.code === code);
 
-        addProduct() {
-            
+        if (existingProduct) {
+            console.log("Ya existe un producto con este codigo.");
+            return;
+        }
+
+        const newProduct = {
+            id: this.productIdCounter++,
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock
+        };
+
+        this.products.push(newProduct);
+        console.log("Producto agregado correctamente:", newProduct);
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const product = this.products.find(product => product.id === id);
+
+        if (product) {
+            return product;
+        } else {
+            console.log("Not found")
         }
     }
 }
+
+let manager = new ProductManager();
+
+manager.addProduct("Producto1", "Producto1-description", 15.50, "thumbnail-producto1", "001", 120)
+manager.addProduct("Producto2", "Producto2-description", 28.00, "thumbnail-producto2", "002", 75)
+manager.addProduct("Producto3", "Producto3-description", 7.80, "thumbnail-producto3", "003", 185)
+manager.addProduct("Producto4", "Producto4-description", 89.80, "thumbnail-producto4", "003", 178)
+
+console.log(manager.getProducts());
+console.log(manager.getProductById(2));
+console.log(manager.getProductById(4));
