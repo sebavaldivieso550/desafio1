048a9de4a29e76ce753a92a4ee643e616f6a00ca
sebavaldivieso550 <sebavaldivieso550@gmail.com>
2024-02-29 23:34:13 -0300

    Desafio 3 listo

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ProductManager = require('../productManager');
+
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json());
+
+const productManager = new ProductManager();
+
+app.get('/products', async (req, res) => {
+   
+    await productManager.loadProducts();
+
+    const limit = req.query.limit ? parseInt(req.query.limit) : null;
+
+    let products = productManager.getProducts();
+
+    if (limit) {
+        products = products.slice(0, limit);
+    }
+
+    res.json(products);
+
+});
+
+app.get('/products/:pid',(req, res) => {
+    const { pid } = req.params;
+    const product = productManager.getProductById(parseInt(pid));
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
